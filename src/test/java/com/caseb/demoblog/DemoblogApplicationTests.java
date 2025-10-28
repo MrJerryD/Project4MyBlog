5f5e8831ea9dc7bb26f688077f68eaf523adbca6
MrJerry <my_main.com>
2025-10-28 17:35:08 +0200

    1 add reps, prepared

---
+++ src/test/java/com/caseb/demoblog/DemoblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caseb.demoblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
